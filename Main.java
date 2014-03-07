84a89e922d37cfa0dc893859acdc9a54850723b5
atish <7705506+atishh@users.noreply.github.com>
2014-03-07 18:39:48 +0530

    Modifying files to comply with latest stanford nlp release

--- Main.java
+++ Main.java
@@ -33,7 +33,7 @@ import edu.stanford.nlp.io.*;
 import edu.stanford.nlp.ling.*;
 import edu.stanford.nlp.pipeline.*;
 import edu.stanford.nlp.trees.*;
-import edu.stanford.nlp.trees.semgraph.SemanticGraph;
+import edu.stanford.nlp.semgraph.SemanticGraph;
 import edu.stanford.nlp.util.*;
 
 
