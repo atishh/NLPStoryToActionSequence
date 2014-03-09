464395d69242c08f9fd59d3cab3cfea3ff9ff7aa
atish <7705506+atishh@users.noreply.github.com>
2014-03-10 00:57:12 +0530

    Adding process background class on similar line of process action

---
+++ ProcessBackground.java
@@ -0,0 +1,71 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.Map;
+import java.util.*;
+
+public class ProcessBackground
+{
+
+  public static String processBackgroundTypeCamp()
+  {
+     String OutFile = "Background: ";
+     OutFile += "camp ";
+     OutFile += " \n";
+     return OutFile;
+  }
+
+  public static String processBackgroundTypeLiving()
+  {
+     String OutFile = "Background: ";
+     OutFile += "live ";
+     Node root = g.root;
+     for (Edge e : root.outEdges)
+     {
+	if((e.label.equalsIgnoreCase("prep_in")) || (e.label.equalsIgnoreCase("prep_by")))
+        {
+            Node BackgroundNode = e.target;
+	    OutFile += BackgroundNode.lex;
+	    OutFile += " ";
+        }
+     }
+     OutFile += " \n";
+     return OutFile;
+  }
+
+
+  public static Map<String, Runnable> processBackgroundMap = new HashMap<String, Runnable>();
+  public static Graph g;
+  public static String OutString;
+  public static boolean bBackgroundNotSupported;
+ 
+  public static void init()
+  {
+	//processBackgroundMap.put("walk", ProcessAction::processActionTypeWalk);
+	processBackgroundMap.put("camp", new Runnable() { public void run() { OutString = processBackgroundTypeCamp(); } });
+	processBackgroundMap.put("camping", new Runnable() { public void run() { OutString = processBackgroundTypeCamp(); } });
+
+	processBackgroundMap.put("living", new Runnable() { public void run() { OutString = processBackgroundTypeLiving(); } });
+	processBackgroundMap.put("live", new Runnable() { public void run() { OutString = processBackgroundTypeLiving(); } });
+  }
+ 
+  public static void ProcessBackgroundLookUp(String BackgroundType)
+  {
+	OutString = "";
+        bBackgroundNotSupported = false;
+	Runnable r = processBackgroundMap.get(BackgroundType.toLowerCase());
+	if (r != null)
+	{
+	    r.run();
+	}
+	else
+	{
+            bBackgroundNotSupported = true;
+	    System.out.println("Currently Background Type "+BackgroundType+" is not supported");
+	}
+  }
+
+}
